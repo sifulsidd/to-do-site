770a19f686a4b53764562f6c438f22e3d91f1b72
Siful <sifulsiddiki@yahoo.com>
2023-05-25 13:32:53 -0400

    Created barebones to-do-list site in order to learn DOM manipulation

---
+++ html/main.js
@@ -0,0 +1,13 @@
+// const button = document.getElementById('add2');
+// const myList = document.getElementById('items');
+
+// function add_To_List(){
+//     const text = document.getElementById('textbox').value;
+//     if(text.length>0){
+//         const newItem = document.createElement('li');
+//         newItem.textContent = text;
+//         myList.appendChild(newItem);
+//     }
+// }
+
+// button.addEventListener('click', add_To_List);
